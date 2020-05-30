ee0a5225e910a7d998ac80ad7a283d1da15981ba
Dzmitry Parkhomenka <Dzmitry_parkhomenka@epam.com>
2020-05-30 15:08:28 +0300

    added game and window classes

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,89 @@
-public class Game {
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+
+public class Game extends Canvas implements Runnable{
+
+    public static int WIDTH = 800, HEIGHT = 608;
+    public String title = "Shooting Game";
+
+    private Thread thread;
+    private boolean isRunning = false;
+
+    public Game() {
+        new Window(WIDTH, HEIGHT, title, this);
+        start();
+    }
+
+    private synchronized void start() {
+        if(isRunning) return;
+
+        thread = new Thread(this);
+        thread.start();
+        isRunning = true;
+    }
+
+    private synchronized void stop() {
+        if(!isRunning) return;
+
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        isRunning = false;
+    }
+
+    //gameloop
+    public void run() {
+        this.requestFocus();
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        long timer = System.currentTimeMillis();
+        int frames = 0;
+        while(isRunning) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            while(delta >= 1) {
+                tick();
+                delta--;
+            }
+            render();
+            frames++;
+
+            if (System.currentTimeMillis() - timer > 1000) {
+                timer += 1000;
+                frames = 0;
+            }
+        }
+        stop();
+    }
+
+    private void tick() {
+        //Update the game
+    }
+
+    private void render() {
+        //Render the game
+        BufferStrategy bufferStrategy = this.getBufferStrategy();
+        if(bufferStrategy == null) {
+            this.createBufferStrategy(3);
+            return;
+        }
+
+        Graphics graphics = bufferStrategy.getDrawGraphics();
+
+        graphics.setColor(Color.BLACK);
+        graphics.fillRect(0, 0, WIDTH, HEIGHT);
+
+        //Rendering happens here
+        bufferStrategy.show();
+        graphics.dispose();
+    }
+
+    public static void main(String[] args) {
+        new Game();
+    }
 }
